65b3321ec09f5ce009d40a6c1e16cbad7ee44557
LePigeonDev <lepigeon.dev@gmail.com>
2025-10-05 16:18:08 +0200

    BUILD SUCCESSFUL

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,22 @@
 public class CarsAssemble {
+    private final int productionPerHour = 221;
+    private double successRate;
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+        if (speed <= 4) {
+            successRate = 1;
+        } else if (speed <= 8) {
+            successRate = 0.90;
+        } else if (speed == 9) {
+            successRate = 0.80;    
+        } else {
+            successRate = 0.77;
+        }
+        
+        return (speed * productionPerHour) * successRate;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        return (int) (productionRatePerHour(speed) / 60);
     }
 }

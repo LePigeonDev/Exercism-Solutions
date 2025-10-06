7f6c03603ff50c7b88a424bcfa5efc0d659d8483
LePigeonDev <lepigeon.dev@gmail.com>
2025-10-06 17:44:39 +0200

    BUILD SUCCESSFUL

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,28 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LogLevels {
     
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLevels.message() method");
+        return logLine.replaceAll("\\[.*?\\:", "").trim();
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLevels.logLevel() method");
+        return getBetweenRegex(logLine, "\\[(.*?)\\]");
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLevels.reformat() method");
+        String brackets = logLevel(logLine);
+        String info = message(logLine);
+        return info + " (" + brackets + ")";
+    }
+
+    private static String getBetweenRegex(String logLine, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(logLine);
+        if(matcher.find()) {
+            return matcher.group(1).toLowerCase();
+        }
+        return null;
     }
 }
--- java/log-levels/src/test/java/LogLevelsTest.java
+++ java/log-levels/src/test/java/LogLevelsTest.java
@@ -7,56 +7,56 @@ import static org.assertj.core.api.Assertions.*;
 public class LogLevelsTest {
     @Test
     @Tag("task:1")
-    @DisplayName("The message method returns the log line's message of an error log")
+    @DisplayName("task:1 :The message method returns the log line's message of an error log")
     public void errorMessage() {
         assertThat(LogLevels.message("[ERROR]: Stack overflow")).isEqualTo("Stack overflow");
     }
 
     @Test
     @Tag("task:1")
-    @DisplayName("The message method returns the log line's message of a warning log")
+    @DisplayName("task:1 :The message method returns the log line's message of a warning log")
     public void warningMessage() {
         assertThat(LogLevels.message("[WARNING]: Disk almost full")).isEqualTo("Disk almost full");
     }
 
     @Test
     @Tag("task:1")
-    @DisplayName("The message method returns the log line's message of an info log")
+    @DisplayName("task:1 :The message method returns the log line's message of an info log")
     public void infoMessage() {
         assertThat(LogLevels.message("[INFO]: File moved")).isEqualTo("File moved");
     }
 
     @Test
     @Tag("task:1")
-    @DisplayName("The message method returns the log line's message after removing leading and trailing spaces")
+    @DisplayName("task:1 :The message method returns the log line's message after removing leading and trailing spaces")
     public void messageWithLeadingAndTrailingWhiteSpace() {
         assertThat(LogLevels.message("[WARNING]:   \tTimezone not set  \r\n")).isEqualTo("Timezone not set");
     }
 
     @Test
     @Tag("task:2")
-    @DisplayName("The logLevel method returns the log level of an error log line")
+    @DisplayName("task:2 :The logLevel method returns the log level of an error log line")
     public void errorLogLevel() {
         assertThat(LogLevels.logLevel("[ERROR]: Disk full")).isEqualTo("error");
     }
 
     @Test
     @Tag("task:2")
-    @DisplayName("The logLevel method returns the log level of a warning log line")
+    @DisplayName("task:2 :The logLevel method returns the log level of a warning log line")
     public void warningLogLevel() {
         assertThat(LogLevels.logLevel("[WARNING]: Unsafe password")).isEqualTo("warning");
     }
 
     @Test
     @Tag("task:2")
-    @DisplayName("The logLevel method returns the log level of an info log line")
+    @DisplayName("task:2 :The logLevel method returns the log level of an info log line")
     public void infoLogLevel() {
         assertThat(LogLevels.logLevel("[INFO]: Timezone changed")).isEqualTo("info");
     }
 
     @Test
     @Tag("task:3")
-    @DisplayName("The reformat method correctly reformats an error log line")
+    @DisplayName("task:3 :The reformat method correctly reformats an error log line")
     public void errorReformat() {
         assertThat(LogLevels.reformat("[ERROR]: Segmentation fault"))
             .isEqualTo("Segmentation fault (error)");
@@ -64,7 +64,7 @@ public class LogLevelsTest {
 
     @Test
     @Tag("task:3")
-    @DisplayName("The reformat method correctly reformats a warning log line")
+    @DisplayName("task:3 :The reformat method correctly reformats a warning log line")
     public void warningReformat() {
         assertThat(LogLevels.reformat("[WARNING]: Decreased performance"))
             .isEqualTo("Decreased performance (warning)");        
@@ -72,7 +72,7 @@ public class LogLevelsTest {
 
     @Test
     @Tag("task:3")
-    @DisplayName("The reformat method correctly reformats an info log line")
+    @DisplayName("task:3 :The reformat method correctly reformats an info log line")
     public void infoReformat() {
         assertThat(LogLevels.reformat("[INFO]: Disk defragmented"))
             .isEqualTo("Disk defragmented (info)");        
@@ -80,7 +80,7 @@ public class LogLevelsTest {
 
     @Test
     @Tag("task:3")
-    @DisplayName("The reformat method correctly reformats an error log line removing spaces")
+    @DisplayName("task:3 :The reformat method correctly reformats an error log line removing spaces")
     public void reformatWithLeadingAndTrailingWhiteSpace() {
         assertThat(LogLevels.reformat("[ERROR]: \t Corrupt disk\t \t \r\n"))
             .isEqualTo("Corrupt disk (error)");        

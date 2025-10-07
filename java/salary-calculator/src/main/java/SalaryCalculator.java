191e51ad3224c97c314d9ec88c4a56d88573bc0b
LePigeonDev <lepigeon.dev@gmail.com>
2025-10-07 11:00:41 +0200

    BUILD SUCCESSFUL

--- java/salary-calculator/src/main/java/SalaryCalculator.java
+++ java/salary-calculator/src/main/java/SalaryCalculator.java
@@ -1,17 +1,21 @@
 public class SalaryCalculator {
+    private static final double _BASESALARY_ = 1000.00;
+    private static final double _SALARYCAP_ = 2000.00;
+
     public double salaryMultiplier(int daysSkipped) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.salaryMultiplier() method");
+        return (daysSkipped >= 5) ? 0.85 : 1.0;
     }
 
     public int bonusMultiplier(int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.bonusMultiplier() method");
+        return (productsSold >= 20) ? 13 : 10;
     }
 
     public double bonusForProductsSold(int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.bonusForProductsSold() method");
+        return bonusMultiplier(productsSold) * productsSold;
     }
 
     public double finalSalary(int daysSkipped, int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.finalSalary() method");
+        double salary = (salaryMultiplier(daysSkipped) * _BASESALARY_) + bonusForProductsSold(productsSold);
+        return (salary > _SALARYCAP_) ? _SALARYCAP_ : salary;
     } 
 }

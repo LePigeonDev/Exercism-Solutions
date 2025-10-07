47546c92eb2731dae8ee0ea055939056803c598f
LePigeonDev <lepigeon.dev@gmail.com>
2025-10-07 11:54:51 +0200

    BUILD SUCCESSFUL

--- java/football-match-reports/src/main/java/FootballMatchReports.java
+++ java/football-match-reports/src/main/java/FootballMatchReports.java
@@ -1,5 +1,36 @@
 public class FootballMatchReports {    
     public static String onField(int shirtNum) {
-        throw new UnsupportedOperationException("Please implement the (static) FootballMatchReports.onField() method");
+        String description;
+        switch (shirtNum) {
+            case 1:
+                description = "goalie";
+                break;
+            case 2:
+                description = "left back";
+                break;
+            case 3,4:
+                description = "center back";
+                break;
+            case 5:
+                description = "right back";
+                break;
+            case 6,7,8:
+                description = "midfielder";
+                break;
+            case 9:
+                description = "left wing";
+                break;
+            case 10:
+                description = "striker";
+                break;
+            case 11:
+                description = "right wing";
+                break;
+            default :
+                description = "invalid";
+                break;
+        }
+        
+        return description;
     }
 }
--- java/football-match-reports/src/test/java/FootballMatchReportsTest.java
+++ java/football-match-reports/src/test/java/FootballMatchReportsTest.java
@@ -8,28 +8,28 @@ public class FootballMatchReportsTest {
 
     @Test
     @Tag("task:1")
-    @DisplayName("The onField method returns the correct description of player with shirt number 1")
+    @DisplayName("task:1 :The onField method returns the correct description of player with shirt number 1")
     public void testGoal() {
         assertThat(FootballMatchReports.onField(1)).isEqualTo("goalie");
     }
 
     @Test
     @Tag("task:1")
-    @DisplayName("The onField method returns the correct description of player with shirt number 2")
+    @DisplayName("task:1 :The onField method returns the correct description of player with shirt number 2")
     public void testLeftBack() {
         assertThat(FootballMatchReports.onField(2)).isEqualTo("left back");
     }
 
     @Test
     @Tag("task:1")
-    @DisplayName("The onField method returns the correct description of player with shirt number 5")
+    @DisplayName("task:1 :The onField method returns the correct description of player with shirt number 5")
     public void testRightBack() {
         assertThat(FootballMatchReports.onField(5)).isEqualTo("right back");
     }
 
     @Test
     @Tag("task:1")
-    @DisplayName("The onField method returns the correct description of players with shirt numbers 3 and 4")
+    @DisplayName("task:1 :The onField method returns the correct description of players with shirt numbers 3 and 4")
     public void testCenterBack() {
         assertThat(FootballMatchReports.onField(3)).isEqualTo("center back");
         assertThat(FootballMatchReports.onField(4)).isEqualTo("center back");
@@ -37,7 +37,7 @@ public class FootballMatchReportsTest {
 
     @Test
     @Tag("task:1")
-    @DisplayName("The onField method returns the correct description of players with shirt numbers 6, 7 and 8")
+    @DisplayName("task:1 :The onField method returns the correct description of players with shirt numbers 6, 7 and 8")
     public void testMidfielder() {
         assertThat(FootballMatchReports.onField(6)).isEqualTo("midfielder");
         assertThat(FootballMatchReports.onField(7)).isEqualTo("midfielder");
@@ -46,35 +46,35 @@ public class FootballMatchReportsTest {
 
     @Test
     @Tag("task:1")
-    @DisplayName("The onField method returns the correct description of player with shirt number 9")
+    @DisplayName("task:1 :The onField method returns the correct description of player with shirt number 9")
     public void testLeftWing() {
         assertThat(FootballMatchReports.onField(9)).isEqualTo("left wing");
     }
 
     @Test
     @Tag("task:1")
-    @DisplayName("The onField method returns the correct description of player with shirt number 10")
+    @DisplayName("task:1 :The onField method returns the correct description of player with shirt number 10")
     public void testStriker() {
         assertThat(FootballMatchReports.onField(10)).isEqualTo("striker");
     }
 
     @Test
     @Tag("task:1")
-    @DisplayName("The onField method returns the correct description of player with shirt number 11")
+    @DisplayName("task:1 :The onField method returns the correct description of player with shirt number 11")
     public void testRightWing() {
         assertThat(FootballMatchReports.onField(11)).isEqualTo("right wing");
     }
 
     @Test
     @Tag("task:2")
-    @DisplayName("The onField method returns 'invalid' for invalid shirt number")
+    @DisplayName("task:2 :The onField method returns 'invalid' for invalid shirt number")
     public void testException() {
         assertThat(FootballMatchReports.onField(13)).isEqualTo("invalid");
     }
 
     @Test
     @Tag("task:2")
-    @DisplayName("The onField method returns 'invalid' for negative shirt number")
+    @DisplayName("task:2 :The onField method returns 'invalid' for negative shirt number")
     public void testExceptionNegativeNumber() {
         assertThat(FootballMatchReports.onField(-1)).isEqualTo("invalid");
     }

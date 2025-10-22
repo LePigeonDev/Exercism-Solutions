29b6ebe4d2f54561ee85a62588f14a8fecedc626
LePigeonDev <lepigeon.dev@gmail.com>
2025-10-22 15:12:14 +0200

    BUILD SUCCESSFUL

--- kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
+++ kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
@@ -1,5 +1,19 @@
 object CollatzCalculator {
     fun computeStepCount(start: Int): Int {
-        TODO("Implement the function to complete the task")
+        require(start > 0) {"[ERROR]: negative numbers"}
+
+        var number : ULong = start.toULong()
+        var count : Int = 0
+
+        while (number != 1UL) {
+            if(number % 2UL == 0UL) {
+                number = number / 2UL
+            } else {
+                number = 3UL * number + 1UL;
+            }
+            count ++;
+        }
+
+        return count;
     }
 }

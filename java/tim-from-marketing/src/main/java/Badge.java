9638659d67e20cd20915da89205cb4567352f9c4
LePigeonDev <lepigeon.dev@gmail.com>
2025-11-24 19:18:23 +0100

    BUILD SUCCESSFUL

--- java/tim-from-marketing/src/main/java/Badge.java
+++ java/tim-from-marketing/src/main/java/Badge.java
@@ -1,16 +1,13 @@
 class Badge {
     public String print(Integer id, String name, String department) {
-        String departmentName =" ";
         String idString = "";
         if (id != null) {
             idString = "[" + id + "] - ";
         }
         if (department == null) {
-            departmentName = "OWNER";
-        } else {
-            departmentName = department.toUpperCase();
-        }
-
-        return idString + name + " - " + departmentName;
+            department = "OWNER";
+        } 
+        
+        return idString + name + " - " + department.toUpperCase();
     }
 }

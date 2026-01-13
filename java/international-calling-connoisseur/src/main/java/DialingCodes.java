351ceb1401ae037958d3a58a2562a4bc2a809ab0
lepigeon.dev <lepigeon.dev@gmail.com>
2026-01-13 19:02:59 +0100

    BUILD SUCCESSFUL

--- java/international-calling-connoisseur/src/main/java/DialingCodes.java
+++ java/international-calling-connoisseur/src/main/java/DialingCodes.java
@@ -1,34 +1,39 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class DialingCodes {
 
+    private Map<Integer, String> dialingCode = new HashMap<>();
+
     public Map<Integer, String> getCodes() {
-        throw new UnsupportedOperationException(
-                "Delete this statement and write your own implementation.");
+        return dialingCode;
     }
 
     public void setDialingCode(Integer code, String country) {
-        throw new UnsupportedOperationException(
-                "Delete this statement and write your own implementation.");
+        getCodes().put(code, country);
     }
 
     public String getCountry(Integer code) {
-        throw new UnsupportedOperationException(
-                "Delete this statement and write your own implementation.");
+        return getCodes().get(code);
     }
 
     public void addNewDialingCode(Integer code, String country) {
-        throw new UnsupportedOperationException(
-                "Delete this statement and write your own implementation.");
+        if(!getCodes().containsKey(code) && !getCodes().containsValue(country)) {
+            setDialingCode(code, country);
+        }
     }
 
     public Integer findDialingCode(String country) {
-        throw new UnsupportedOperationException(
-                "Delete this statement and write your own implementation.");
+        for(Integer code : getCodes().keySet()) {
+            if(getCodes().get(code) == country) {
+                return code;
+            }
+        }
+        return null;
     }
 
     public void updateCountryDialingCode(Integer code, String country) {
-        throw new UnsupportedOperationException(
-                "Delete this statement and write your own implementation.");
+        getCodes().remove(findDialingCode(country));
+        setDialingCode(code, country);
     }
 }

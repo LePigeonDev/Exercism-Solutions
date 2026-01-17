c2e50760b40d7a39c507bb4c251341a07cf739f5
lepigeon.dev <lepigeon.dev@gmail.com>
2026-01-17 11:51:36 +0100

    BUILD SUCCESSFUL

--- java/darts/src/main/java/Darts.java
+++ java/darts/src/main/java/Darts.java
@@ -1,5 +1,13 @@
 class Darts {
     int score(double xOfDart, double yOfDart) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        double result = Math.sqrt((xOfDart * xOfDart) + (yOfDart * yOfDart));
+        if (result <= 1) {
+            return 10;
+        } else if (result <= 5) {
+            return 5;
+        } else if (result <= 10) {
+            return 1;
+        }
+        return 0;
     }
 }

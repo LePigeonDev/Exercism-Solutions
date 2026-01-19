6050820296a0c91132000d75ba7060c8b4ce7901
lepigeon.dev <lepigeon.dev@gmail.com>
2026-01-19 10:55:51 +0100

    BUILD SUCCESSFUL

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -3,11 +3,14 @@ import java.math.BigInteger;
 class Grains {
 
     BigInteger grainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if(square < 65 && square > 0) {
+            return BigInteger.ONE.shiftLeft(square - 1);
+        }
+        throw new IllegalArgumentException("square must be between 1 and 64");
     }
 
     BigInteger grainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return BigInteger.ONE.shiftLeft(64).subtract(BigInteger.ONE);
     }
 
 }
